196d7df0c928a13c36852948edc346a46257d82b
Peterson <petersonros@hotmail.com>
2022-02-12 10:10:01 -0300

    desenho do boneco completo

--- assets/canvas.js
+++ assets/canvas.js
@@ -1,30 +1,75 @@
 var tela = document.querySelector("canvas");
-var pincel = tela.getContext('2d');
+var linha = tela.getContext('2d');
 
-pincel.fillStyle = '#252525';
-pincel.fillRect(0, 0, 510, 360);
-pincel.fill();
+linha.fillStyle = '#252525';
+linha.fillRect(0, 0, 510, 360);
+linha.fill();
 
 /* desenho da forca */
-pincel.fillStyle = "#FFD700"
-pincel.beginPath();
-pincel.moveTo(167, 60); /* ponto 1 */
-pincel.lineTo(167, 340); /* ponto 2 */
-pincel.lineTo(105, 355); /* ponto 3 */
-pincel.lineTo(105, 360); /* ponto 4 */
-pincel.lineTo(235, 360); /* ponto 5 */
-pincel.lineTo(235, 355); /* ponto 6 */
-pincel.lineTo(173, 340); /* ponto 7 */
-pincel.lineTo(173, 66); /* ponto 8 */
-pincel.lineTo(334, 66); /* ponto 9 */
-pincel.lineTo(334, 120); /* ponto 10 */
-pincel.lineTo(340, 120); /* ponto 11 */
-pincel.lineTo(340, 60); /* ponto 12 */
-pincel.fill();
+linha.fillStyle = "#FFD700"
+linha.beginPath();
+linha.moveTo(167, 60);
+linha.lineTo(167, 340);
+linha.lineTo(105, 355);
+linha.lineTo(105, 360);
+linha.lineTo(235, 360);
+linha.lineTo(235, 355);
+linha.lineTo(173, 340);
+linha.lineTo(173, 66);
+linha.lineTo(334, 66);
+linha.lineTo(334, 120);
+linha.lineTo(340, 120);
+linha.lineTo(340, 60);
+linha.fill();
 
 /* desenho do boneco */
+/* cabeça */
+linha.fillStyle = '#FFD700';
+linha.beginPath();
+linha.arc(337, 143, 23, 0, 2 * 3.14);
+linha.fill();
 
-/* pincel.fillStyle = 'blue';
-pincel.beginPath();
-pincel.arc(300, 200, 50, 0, 2 * 3.14);
-pincel.fill(); */
+/* tronco */
+linha.fillStyle = "#FFD700"
+linha.beginPath();
+linha.moveTo(334, 166);
+linha.lineTo(334, 229);
+linha.lineTo(340, 229);
+linha.lineTo(340, 166);
+linha.fill();
+
+/* braço esquerdo */
+linha.fillStyle = "#FFD700"
+linha.beginPath();
+linha.moveTo(337, 170);
+linha.lineTo(337, 178);
+linha.lineTo(310, 205);
+linha.lineTo(310, 197);
+linha.fill();
+
+/* braço direito */
+linha.fillStyle = "#FFD700"
+linha.beginPath();
+linha.moveTo(337, 170);
+linha.lineTo(337, 178);
+linha.lineTo(364, 205);
+linha.lineTo(364, 197);
+linha.fill();
+
+/* perna esquerda */
+linha.fillStyle = "#FFD700"
+linha.beginPath();
+linha.moveTo(337, 218);
+linha.lineTo(337, 226);
+linha.lineTo(310, 253);
+linha.lineTo(310, 245);
+linha.fill();
+
+/* perna direita */
+linha.fillStyle = "#FFD700"
+linha.beginPath();
+linha.moveTo(337, 218);
+linha.lineTo(337, 226);
+linha.lineTo(364, 253);
+linha.lineTo(364, 245);
+linha.fill();
